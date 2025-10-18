a436eb5b461175dbf566679096ddf1a8064d88dd
alexvas12 <alexva1374@gmail.com>
2025-10-18 03:04:07 -0400

    updated todo list with Bootstrap instead of css. And renaming hobby app

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [toDos, setToDos] = useState([])
@@ -23,19 +20,19 @@ function App() {
 
   return (
     <>
-      <div className="to-do">
+      <div className="mt-4 d-flex align-items-center flex-column border border-black border-2 p-4 w-50 mx-auto shadow-lg rounded bg-primary">
         <h1>The Important To-Do List</h1>
-        
         <div>
           <input 
+          className = "shadow rounded-2 p-1 border-2 border-black"
             type="text"
             value={input}
             placeholder="Please Add a new Task"
             onChange={(e) => setInput(e.target.value)}
           >
           </input>
-          <button 
-            className="add_button"
+          <button
+            className="m-2 btn btn-success text-black fw-bold shadow-lg rounded-4"
             onClick={add_task}>
             Add Task
           </button>
@@ -44,19 +41,16 @@ function App() {
         <ol>
           {toDos.map((toDo, id) => (
             <li key={id}>
-              {toDo}
-              <button 
-              className="delete_button"
+              <div className="d-flex justify-content-left text-black fw-bold m-2">{toDo}
+              <button className=" mx-2 button bg-danger text-white rounded border-2 align-items-center d-flex shadow-lg rounded-4"
               onClick={() => delete_task(id)}>
               Delete
-              </button>
+              </button></div>
             </li>
           ))}
-          
         </ol>
       </div>
     </>
   )
 }
-
 export default App
